ce49be55b378d1b90173e10d6f435670b4055650
Alejandro Gonzalez <wilson1234567891962@hotmail.com>
2024-01-15 12:47:56 -0500

    Update the code

--- dist/Button/Button.d.ts
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./Button.css";
-export interface ButtonProps extends React.ComponentProps<"button"> {
-    kind?: "primary" | "secondary";
-}
-export declare const Button: React.ForwardRefExoticComponent<Omit<ButtonProps, "ref"> & React.RefAttributes<HTMLButtonElement>>;
